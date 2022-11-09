af66681e4b4aeafd1a4f9b6164a4f4bfd8212935
Danylo Tulainov <niltul22@gmail.com>
2022-11-09 19:13:28 +0100

    added other files

---
+++ src/main/java/com/hillel/tulainov/homeworks/homework2/MyApp.java
@@ -0,0 +1,13 @@
+package com.hillel.tulainov.homeworks.homework2;
+
+public class MyApp {
+    public static void main(String[] args) {
+
+        String width = "51\u00b010\u203244\u2033";
+        String longitude = "-1\u00b049\u203234\u2033";
+
+        System.out.println("Stonehenge coordinates:");
+        System.out.println(width + ", " + longitude);
+
+    }
+}
