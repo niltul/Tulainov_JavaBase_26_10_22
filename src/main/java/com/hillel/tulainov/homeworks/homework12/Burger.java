86c2c21d2726b8e4bd2e02d27120d05a2fb2817e
Danylo Tulainov <niltul22@gmail.com>
2022-12-28 09:15:23 +0100

    hw 13

---
+++ src/main/java/com/hillel/tulainov/homeworks/homework12/Burger.java
@@ -0,0 +1,56 @@
+package com.hillel.tulainov.homeworks.homework12;
+
+public class Burger {
+
+    String bun;
+    String meat;
+    String mayo;
+    String greens;
+    String secondMeat;
+    boolean isDouble = false;
+    boolean isDiet = false;
+
+    public Burger(String bun, String meat, String mayo, String greens) {
+        this.bun = bun;
+        this.meat = meat;
+        this.mayo = mayo;
+        this.greens = greens;
+
+        print();
+    }
+
+    public Burger(String bun, String meat, String greens) {
+        isDiet = true;
+        this.greens = greens;
+        this.meat = meat;
+        this.bun = bun;
+
+        print();
+    }
+    
+    public Burger(String bun, String meat, String greens, String secondMeat, String mayo) {
+        isDouble = true;
+        this.bun = bun;
+        this.meat = meat;
+        this.greens = greens;
+        this.secondMeat = secondMeat;
+        this.mayo = mayo;
+
+        print();
+    }
+
+    public void print() {
+
+        if (isDouble) {
+            System.out.println("Composition of double burger: " + bun + " " +
+                    meat + " " + mayo + " " + greens + " " + secondMeat);
+        } else if (isDiet) {
+            System.out.println("Composition of burger: " + bun + " " +
+                    meat + " " + greens);
+        } else {
+            System.out.println("Composition of burger: " + bun + " " +
+                    meat + " " + mayo + " " + greens);
+        }
+    }
+
+}
---
+++ src/main/java/com/hillel/tulainov/homeworks/homework12/BurgerMain.java
@@ -0,0 +1,41 @@
+package com.hillel.tulainov.homeworks.homework12;
+
+import java.util.Scanner;
+
+public class BurgerMain {
+    public static void main(String[] args) {
+
+        Burger simpleBurger = new Burger("bun", "beef", "mayo", "greens");
+        Burger dietBurger = new Burger("bun", "beef", "greens");
+        Burger doubleBurger = new Burger("bun", "beef", "greens", "beef", "mayo");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please choose your burger: ");
+        System.out.println("1 - simple");
+        System.out.println("2 - diet");
+        System.out.println("3 - double");
+        int answer;
+
+        while (true) {
+            if (scanner.hasNextInt()) {
+
+                answer = scanner.nextInt();
+                if (answer <= 0 || answer >= 4) {
+                    System.out.println("Wrong number, try again");
+                } else {
+                    break;
+                }
+            } else {
+                System.out.println("Wrong input. try again.");
+                scanner.nextLine();
+            }
+        }
+
+        switch (answer) {
+            case 1: simpleBurger.print(); break;
+            case 2: dietBurger.print(); break;
+            case 3: doubleBurger.print(); break;
+        }
+    }
+}
